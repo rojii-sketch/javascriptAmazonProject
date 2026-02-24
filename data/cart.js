b3420bae08fb3abee5dc78921548265b3eb8707a
rojii-sketch <kahindirodgers77@gmail.com>
2026-02-24 10:18:17 +0300

    Added Delete Interactions

--- data/cart.js
+++ data/cart.js
@@ -1,8 +1,16 @@
-export let cart = [{productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+export let cart = JSON.parse(localStorage.getItem('cart'));
+if(!cart){
+  cart= [{productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2,
-}, {productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+}, {
+  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity: 1
 }];
+}
+
+function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 export function addToCart(productId){
   let matchingItem;
    cart.forEach((item)=>{
@@ -20,6 +28,8 @@ export function addToCart(productId){
    });
    }
 }
+saveToStorage();
+
 export function removeFromCart(productId){
   const newCart = [];
   cart.forEach((cartItem)=>{
@@ -28,4 +38,5 @@ export function removeFromCart(productId){
     }
   });
   cart = newCart;
+  saveToStorage();
 }
