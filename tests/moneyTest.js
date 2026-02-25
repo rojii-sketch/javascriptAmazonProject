cf5057cdcc94569f2f7dd0e13095351eb8c0cb48
rojii-sketch <kahindirodgers77@gmail.com>
2026-02-25 18:27:47 +0300

    Create automated tests for format currency

---
+++ tests/moneyTest.js
@@ -0,0 +1,26 @@
+import {formartCurrency} from '../scripts/utils/money.js';
+console.log('Test suite: Format currency')
+console.log('converets cents into dollars')
+if (formartCurrency(2095) === '20.95'){
+  console.log('passed');
+}else {
+  console.log('failed');
+}
+console.log('Works with zero')
+if (formartCurrency(0)=== '0.00'){
+  console.log('passed')
+}else {
+  console.log('failed');
+}
+console.log('Rounds up to the nearest cents')
+if (formartCurrency(2000.5)==='20.01'){
+  console.log('passed')
+}else {
+  console.log('failed');
+}
+console.log('round up to the nearest cents')
+if (formartCurrency(2000.4)=== '20.00'){
+  console.log('passed')
+}else {
+  console.lof('failed');
+}
