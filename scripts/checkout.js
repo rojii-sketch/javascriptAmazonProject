a2bfcd5531064c93ad828b007372c604333f481c
rojii-sketch <kahindirodgers77@gmail.com>
2026-02-26 19:06:57 +0300

    Add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,6 +2,7 @@
 import { renderPaymentSummary } from './checkout/payementSummary.js';
 import { renderOrderSummary } from './checkout/order.js';
 //import '../data/cart-class.js';
+import '../data/backend-practice.js';
 
 renderOrderSummary();
 renderPaymentSummary();
