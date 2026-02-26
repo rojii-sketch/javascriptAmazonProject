92b7e482c350962cae7fb0305f763dce4faa429a
rojii-sketch <kahindirodgers77@gmail.com>
2026-02-26 19:42:19 +0300

    load products from the backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,8 +1,11 @@
 
 import { renderPaymentSummary } from './checkout/payementSummary.js';
 import { renderOrderSummary } from './checkout/order.js';
+import { loadProducts } from '../data/products.js';
 //import '../data/cart-class.js';
-import '../data/backend-practice.js';
+//import '../data/backend-practice.js';
 
+loadProducts(()=>{
 renderOrderSummary();
 renderPaymentSummary();
+});
