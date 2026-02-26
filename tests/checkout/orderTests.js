2f0903931374d7c4347892c2805510355b99036c
rojii-sketch <kahindirodgers77@gmail.com>
2026-02-26 20:01:22 +0300

    Load products in beforeAll

--- tests/checkout/orderTests.js
+++ tests/checkout/orderTests.js
@@ -1,10 +1,17 @@
 import { renderOrderSummary } from "../../scripts/checkout/order.js";
 import {cart, loadFromStorage } from '../../data/cart.js';
+import { loadProducts } from "../../data/products.js";
+
 
 describe('test suite: renderOrderSummary', ()=>{
    const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d'
     const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6'
-   
+    beforeAll((done)=>{
+      loadProducts(()=>{
+        done();
+      });
+      
+    });
    beforeEach(()=>{
        spyOn(localStorage, 'setItem');
      document.querySelector('.js-test-container').innerHTML = `
