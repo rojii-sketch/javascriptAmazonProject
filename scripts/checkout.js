c1c7d9398e2dd3ccf4668ab04c094039be69edb7
rojii-sketch <kahindirodgers77@gmail.com>
2026-02-26 15:03:29 +0300

    oop

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 
 import { renderPaymentSummary } from './checkout/payementSummary.js';
 import { renderOrderSummary } from './checkout/order.js';
+import '../data/cart-oop.js';
 
 renderOrderSummary();
 renderPaymentSummary();
