c72c51d3f5ceae1390bc072ea51909901629c097
rojii-sketch <kahindirodgers77@gmail.com>
2026-02-26 16:04:19 +0300

    create class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 
 import { renderPaymentSummary } from './checkout/payementSummary.js';
 import { renderOrderSummary } from './checkout/order.js';
-import '../data/cart-oop.js';
+import '../data/cart-class.js';
 
 renderOrderSummary();
 renderPaymentSummary();
