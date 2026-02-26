b28effbd11e134302777688672732a0e193cb2eb
rojii-sketch <kahindirodgers77@gmail.com>
2026-02-26 16:53:45 +0300

    convert products into classes

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 
 import { renderPaymentSummary } from './checkout/payementSummary.js';
 import { renderOrderSummary } from './checkout/order.js';
-import '../data/cart-class.js';
+//import '../data/cart-class.js';
 
 renderOrderSummary();
 renderPaymentSummary();
--- scripts/checkout/order.js
+++ scripts/checkout/order.js
@@ -41,7 +41,7 @@ export function renderOrderSummary (){
                     ${matchingProduct.name}
                   </div>
                   <div class="product-price">
-                    $${(formartCurrency(matchingProduct.priceCents))}
+                    ${matchingProduct.getPrice()}
                   </div>
                   <div class="product-quantity js-product-quantity-${matchingProduct.id}">
                     <span>
