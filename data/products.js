7f80667efb17de9eb4b9d0a7ba1d170822f6cc3d
rojii-sketch <kahindirodgers77@gmail.com>
2026-02-27 11:17:16 +0300

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -87,9 +87,12 @@ export function loadProductsFetch(){
 });
 console.log('load products')
    
+  }).catch((error)=>{
+    console.log('Unexpected error. Please try again later')
   });
   return promise;
 }
+
  /*loadProductsFetch().then(()=>{
   console.log('next step')
 });
@@ -106,7 +109,10 @@ export function loadProducts(fun){
 });
 console.log('load products')
 fun();
- })
+ });
+ xhr.addEventListener('error', (error)=>{
+  console.log('Unexpected error. Please try again later');
+ });
  xhr.open('GET', 'https://supersimplebackend.dev/products');
  xhr.send();
 }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -6,14 +6,22 @@ import {loadCart} from '../data/cart.js';
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 async function loadPage(){
- 
+ try {
+  //throw 'error1'
   await loadProductsFetch();
 
- await new Promise((resolve)=>{
+ const value =await new Promise((resolve, reject)=>{
+  //throw 'error2'
      loadCart(()=>{
-      resolve();
+      resolve('value');
+     // reject('error3')
      });
   });
+
+ } catch(error){
+  console.log('Unexpected error. Please try again later')
+ }
+  
  renderOrderSummary();
  renderPaymentSummary();
   
